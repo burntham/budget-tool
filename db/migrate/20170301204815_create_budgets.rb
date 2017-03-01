262bcc4e240af1122a6d979892cbc73e00db405f
Daniel <danielburnham-king@Daniels-MacBook-Pro.local>
2017-03-01 22:48:56 +0200

    add budget

---
+++ db/migrate/20170301204815_create_budgets.rb
@@ -0,0 +1,10 @@
+class CreateBudgets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :budgets do |t|
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
