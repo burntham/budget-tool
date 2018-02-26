175a97c19f512529810536a313f2eeb76fddb27c
Daniel <drbking@gmail.com>
2018-02-26 20:46:35 +0200

    budget is now sensible

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,8 +1,8 @@
 class BudgetsController < ApplicationController
-  before_action :set_budget_budget, only: [:show, :edit, :update, :destroy]
+  before_action :set_budget, only: [:show, :edit, :update, :destroy]
 
   def index
-    @budget_budgets = Budget.all
+    @budgets = Budget.all
   end
 
   def show
@@ -18,7 +18,7 @@ class BudgetsController < ApplicationController
 
   # GET /budget/budgets/new
   def new
-    @budget_budget = Budget.new
+    @budget = Budget.new
   end
 
   # GET /budget/budgets/1/edit
@@ -28,15 +28,15 @@ class BudgetsController < ApplicationController
   # POST /budget/budgets
   # POST /budget/budgets.json
   def create
-    @budget_budget = Budget.new(budget_budget_params)
+    @budget = Budget.new(budget_params)
 
     respond_to do |format|
-      if @budget_budget.save
-        format.html { redirect_to @budget_budget, notice: 'Budget was successfully created.' }
-        format.json { render :show, status: :created, location: @budget_budget }
+      if @budget.save
+        format.html { redirect_to @budget, notice: 'Budget was successfully created.' }
+        format.json { render :show, status: :created, location: @budget }
       else
         format.html { render :new }
-        format.json { render json: @budget_budget.errors, status: :unprocessable_entity }
+        format.json { render json: @budget.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -45,12 +45,12 @@ class BudgetsController < ApplicationController
   # PATCH/PUT /budget/budgets/1.json
   def update
     respond_to do |format|
-      if @budget_budget.update(budget_budget_params)
-        format.html { redirect_to @budget_budget, notice: 'Budget was successfully updated.' }
-        format.json { render :show, status: :ok, location: @budget_budget }
+      if @budget.update(budget_params)
+        format.html { redirect_to @budget, notice: 'Budget was successfully updated.' }
+        format.json { render :show, status: :ok, location: @budget }
       else
         format.html { render :edit }
-        format.json { render json: @budget_budget.errors, status: :unprocessable_entity }
+        format.json { render json: @budget.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -58,22 +58,22 @@ class BudgetsController < ApplicationController
   # DELETE /budget/budgets/1
   # DELETE /budget/budgets/1.json
   def destroy
-    @budget_budget.destroy
+    @budget.destroy
     respond_to do |format|
-      format.html { redirect_to budget_budgets_url, notice: 'Budget was successfully destroyed.' }
+      format.html { redirect_to budgets_path, notice: 'Budget was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_budget_budget
-      @budget_budget = Budget.find(params[:id])
+    def set_budget
+      @budget = Budget.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def budget_budget_params
-      params.require(:budget_budget).permit(:start_date, :end_date,
+    def budget_params
+      params.require(:budget).permit(:start_date, :end_date,
                                             category_details_attributes: [
                                               :id,
                                               :name,
